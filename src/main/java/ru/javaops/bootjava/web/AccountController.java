12c4b337e321fe0f328451d0b77c35e7a1605588
Валентин <moysait1984@yandex.ru>
2021-05-24 20:45:31 +0400

    4_06_basic_auth_fix.patch

--- src/main/java/ru/javaops/bootjava/web/AccountController.java
+++ src/main/java/ru/javaops/bootjava/web/AccountController.java
@@ -2,11 +2,17 @@ package ru.javaops.bootjava.web;
 
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javaops.bootjava.AuthUser;
+import ru.javaops.bootjava.model.Role;
 import ru.javaops.bootjava.model.User;
+import ru.javaops.bootjava.repository.UserRepository;
+import ru.javaops.bootjava.util.ValidationUtil;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Set;
 
 @RestController
 @RequestMapping(value = "/api/account")
@@ -14,6 +20,7 @@ public class AccountController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User get(@AuthenticationPrincipal AuthUser authUser) {
+        log.info("get {}", authUser);
         return authUser.getUser();
     }
 }
